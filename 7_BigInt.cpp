7e9bfc5202cec5b5b306639efeedb9d6ed889d5f
bala <sokukumar678@gmail.com>
2024-10-07 03:42:19 +0530

    adding two * operator

--- 7_BigInt.cpp
+++ 7_BigInt.cpp
@@ -34,12 +34,46 @@ class BigInt{
     friend bool operator>=(const BigInt &,const BigInt &);
     friend bool operator<=(const BigInt &,const BigInt &);
 
-    friend BigInt &operator*=(BigInt &,const BigInt &);
-    friend BigInt operator*(const BigInt &,const BigInt &);
+    friend BigInt &operator*=(BigInt &, BigInt &);
+    friend BigInt operator*(const BigInt &, BigInt &);
     friend BigInt operator/=(BigInt &,const BigInt &);
     friend BigInt &operator/(const BigInt &,const BigInt &);
     
 }; 
+
+BigInt operator*(const BigInt &a, BigInt &b){
+    BigInt temp;
+    temp=a;
+    temp*=b;
+    return temp;
+}
+
+
+BigInt &operator*=(BigInt &a, BigInt& b){
+    if(Null(a)||Null(b)){
+      a=BigInt();
+      return a;  
+    }
+    int n=a.digits.size(),m=b.digits.size();
+    vector<int>v(n+m,0);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            v[i+j]+=(a.digits[i])*(b.digits[j]);
+        }
+    }
+    n+=m;
+    a.digits.resize(v.size());
+    for(int s,i=0,t=0;i<n;i++){
+        s=t+v[i];
+        v[i]=s%10;
+        t=s/10;
+        a.digits[i]=v[i];
+    }
+    for(int i=n-1;i>=1&&!v[i];i--){
+        a.digits.pop_back();
+    }
+    return a;
+}
 
 BigInt &operator+=(BigInt &a,const BigInt &b){
     int t=0,s,i;
